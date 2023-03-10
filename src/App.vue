fecfd131bd063e4873639e372b12c9bdc2be8538
LucasLiebl <lucas.lliebl@gmail.com>
2023-03-10 16:54:46 -0300

    aula2

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <script setup>
-import{ ref } from `vue`
-const contador = ref (10)
+import { ref } from 'vue'
+const contador = ref(10)
 
 function incrementar() {
     contador.value++
 }
 function decrementar() {
-    if(contador.value > 0){
+    if (contador.value > 0) {
         contador.value--
     }
 }
-function resetar(){
+function resetar() {
     contador.value = 0
 }
+const ok = contador.value > 10
+
 </script>
 <template>
     <button @click="incrementar">+</button>
     <button @click="decrementar">-</button>
     <button @click="resetar">C</button>
     <p>Valor do contador: {{ contador }}</p>
-  </template>
+    <div v-if="ok">Mostrar</div>
+</template>
   
-  <style scoped>
-  button {
+<style scoped>
+button {
     margin-left: 10px;
-    background-color: rgb(22, 86, 214);
+    background-color: rgb(202, 12, 155);
     color: aliceblue;
     border: none;
     border-radius: 5px;
     padding: 5px 10px;
     cursor: pointer;
-  }
-  </style>
+}
+</style>
