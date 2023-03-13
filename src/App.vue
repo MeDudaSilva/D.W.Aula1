f8931eaf03a5bf4f39ea959dd1cc93a366086ef5
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2023-03-13 11:50:37 -0300

    correção-aula2

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,65 @@
 <script setup>
-import { ref } from 'vue'
-const contador = ref(10)
+import { ref } from 'vue';
+const nome = 'Maria Eduarda'
+const idade = 16
+const contador= ref(0)
 
-function incrementar() {
-    contador.value++
+function inverter(texto) {
+    return texto.split('').reverse().join('')
 }
-function decrementar() {
-    if (contador.value > 0) {
-        contador.value--
-    }
+
+function inverterMaiusculo(texto) {
+    return inverter(texto).toUpperCase()
 }
-function resetar() {
-    contador.value = 0
+
+function saudacao() {
+    return `Olá, ${nome}!`
 }
-const ok = contador.value > 10
 
 </script>
+
 <template>
-    <button @click="incrementar">+</button>
-    <button @click="decrementar">-</button>
-    <button @click="resetar">C</button>
-    <p>Valor do contador: {{ contador }}</p>
-    <div v-if="ok">Mostrar</div>
+    <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }} anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
+        <p>O nome tem {{ nome.length }} caracteres</p>
+        <p>O nome invertido é: {{ inverter(nome) }} </p>
+        <p>O nome invertido e em maiusculo é: {{ inverterMaiusculo(nome) }} </p>
+        <hr />
+        <p> Exemplo de saudação usando função</p>
+        <p> {{ saudacao() }} </p>
+    </div>
+    <div class="info">
+        contador: {{ contador }}
+        <button @click="contador++">+</button>
+        <button @click="contador--">-</button>
+        <div v-if="contador > 10 " class="verde">O contador é maior que 10</div>
+        <div v-else-if="contador <   10" class="vermelho">o contador é menor que 10</div>
+        <div v-else>o contador é 10</div>
+    </div>
 </template>
-  
+
 <style scoped>
-button {
-    margin-left: 10px;
-    background-color: rgb(202, 12, 155);
-    color: aliceblue;
-    border: none;
-    border-radius: 5px;
-    padding: 5px 10px;
-    cursor: pointer;
+.info {
+    background-color: rgba(35, 12, 22, 1);
+    padding: 20px 30px;
+    color: rgb(165, 165, 165);
+    border-radius: 10px;
+    margin: 20px;
+}
+
+h1 {
+    color: white;
+    font-weight: bold;
+    font-size: 1.5em;
+}
+.verde {
+    color: green;
+}
+.vermelho{
+    color: red;
 }
 </style>
